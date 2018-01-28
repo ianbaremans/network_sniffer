436831161da63158dc040a04d5063eef7d57cfeb
ianbaremans <ianbaremans@gmail.com>
2018-01-28 16:34:55 +0100

    Seperate adress and bytes

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import socket
 
 #create an INET, raw socket
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
- 
+
 # receive a packet
 while True:
-       print(s.recvfrom(65565))
+    raw_output = s.recvfrom(65565)
+    bytes_raw_output = raw_output[0]
+    bytes_output = str(bytes_raw_output).encode("utf-8").strip()
+    adress_raw_output = raw_output[1]
+
+    print(bytes_output)
+
